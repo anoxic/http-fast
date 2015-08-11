3b88bc43c622ada28b5c131810e1ef09e7bc81e2
anoxic <brian@zickzickzick.com>
2015-08-11 14:09:58 -0700

    use status code

--- http.php
+++ http.php
@@ -9,7 +9,9 @@ function http($method, $uri, $content = null, $header = null, $other_options = n
     $stream = fopen($uri, 'r', false, stream_context_create($options));
     $headers = stream_get_meta_data($stream)['wrapper_data'];
     $body = stream_get_contents($stream);
+    $status_code = count($headers) ? substr($headers[0], 9, 3) : false;
 
-    return [fclose($stream), $headers, $body];
+    fclose($stream); 
+    return [$status_code, $headers, $body];
 }
 
